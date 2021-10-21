102a4add136e9fe5722d1035125fdeae552aa502
Sophieeeeeeeee <sophiesun2810@gmail.com>
2021-10-21 12:08:39 -0400

    Add button second attempt ORZ

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+//
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         {/*<p>*/}
+//         {/*  Edit <code>src/App.js</code> and save to reload.*/}
+//         {/*</p>*/}
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      username: ''
+    }
+
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick () {
+    axios.get('https:////cors-anywhere.herokuapp.com/http://ec2-3-144-10-133.us-east-2.compute.amazonaws.com/')
+        .then(response => this.setState({username: response.data.name}))
+  }
+
+  render () {
+    return (
+        <div className='button__container'>
+          <button className='button' onClick={this.handleClick}>Click Me</button>
+          <p>{this.state.username}</p>
+        </div>
+    )
+  }
 }
+export default App
 
-export default App;
+
