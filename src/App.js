b6b6da7ddc25d60f830921ecb2d0df19bc6e9110
Sophieeeeeeeee <50003926+Sophieeeeeeeee@users.noreply.github.com>
2021-10-22 10:21:20 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,75 +1,43 @@
-// import logo from './logo.svg';
-import './App.css';
-import axios from "axios";
-import {Component} from "react";
-//
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         {/*<p>*/}
-//         {/*  Edit <code>src/App.js</code> and save to reload.*/}
-//         {/*</p>*/}
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+//import axios from "axios";
+import React, {Component} from "react";
+import XMLParser from 'react-xml-parser';
+import {parse} from "fast-xml-parser";
 
 class App extends Component {
-  constructor () {
-    super()
-    this.state = {
-      username: ''
+    constructor (props) {
+        super(props)
+        this.state = {
+            message: ''
+        }
+        this.handleClick = this.handleClick.bind(this)
     }
 
-    this.handleClick = this.handleClick.bind(this)
-  }
+    handleClick () {
+        var xhr = new XMLHttpRequest()
+        var xmlDoc;
+        xhr.addEventListener('load', () => {
+          // update the state of the component with the result here
+          console.log(xhr.responseText)
+            xmlDoc = parse(xhr.responseText);
+        })
+        xhr.open('GET', 'https://cors-anywhere.herokuapp.com/http://ec2-18-118-163-255.us-east-2.compute.amazonaws.com:8080/senso')
+        xhr.send()
 
-  handleClick () {
-    // var xhr = new XMLHttpRequest()
-    // xhr.addEventListener('load', () => {
-    //   // update the state of the component with the result here
-    //   console.log(xhr.responseText)
-    // })
-    // xhr.open('GET', 'https:////cors-anywhere.herokuapp.com/https://66wy5dn7i7.execute-api.us-east-2.amazonaws.com/test/helloworld?name=John&city=Seattle')
-    // xhr.send()
-    // this.setState({username: xhr.responseText})
+        this.setState({message: xmlDoc})
 
-    // axios.get('https:////cors-anywhere.herokuapp.com/http://ec2-3-144-10-133.us-east-2.compute.amazonaws.com/')
-    //     .then(response => this.setState({username: response.data.name}))
+        // axios.get('https:////cors-anywhere.herokuapp.com/https://ge3dewpamf.execute-api.us-east-2.amazonaws.com/test/helloworld?name=John&city=Seattle')
+        //     .then(response => this.setState({username: response.data.name}))
 
-    // axios.get('https:////cors-anywhere.herokuapp.com/https://github.com/Sophieeeeeeeee')
-    //     .then(response => this.setState({username: response.data.name}))
-    // works!
+    }
 
-    axios.get('https:////cors-anywhere.herokuapp.com/https://ge3dewpamf.execute-api.us-east-2.amazonaws.com/test/helloworld?name=John&city=Seattle')
-        .then(response => this.setState({username: response.data.name}))
-
-
-    // axios.get('http://ec2-3-144-10-133.us-east-2.compute.amazonaws.com/')
-    //        .then(response => console.log(response))
-
-
-
-  }
-
-  render () {
-    return (
-        <div className='button__container'>
-          <button className='button' onClick={this.handleClick}>Click Me</button>
-          <p>{this.state.username}</p>
-        </div>
-    )
-  }
+    render () {
+        return (
+            <div className='button__container'>
+                <button className='button' onClick={this.handleClick}>Click Me</button>
+                <h2>{this.state.message}</h2>
+            </div>
+        )
+    }
 }
 export default App
 
