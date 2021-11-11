cadef88edb4c0e51784d7e9ee176d2f9fa458f6a
Sophieeeeeeeee <50003926+Sophieeeeeeeee@users.noreply.github.com>
2021-11-12 03:28:33 +0800

    Create Cars.js

---
+++ src/components/Cars.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import './Cars.css';
+import CardItem from './CarItem';
+import CarFilter from'./CarFilter'
+
+function Cars(props) {
+
+    const getAllCars= () =>{
+
+
+    }
+    return (
+        <div className='cards'>
+
+            <CarFilter
+            color = {props.carColor}
+            type = {props.carType}
+            brand = {props.carBrand}
+            age = {props.carAge}
+            handleCarFilter = {props.handleCarFilter}
+            sendPost = {props.sendPost}
+            />
+
+            <h1>Check out these cars recommended to you!</h1>
+            <h1>Apply filter to find the one for you!</h1>
+
+            {/*cars rendered below should be based on car filter (and sign up info from before)*/}
+            <div className='cards__container'>
+                <div className='cards__wrapper'>
+
+                    <ul className='cards__items'>
+                        <CardItem
+                            src='/images/car1.jpeg'
+                            text='Second hand BMW'
+                            label='Half price!'
+                            path='/services'
+                        />
+                        <CardItem
+                            src='images/car2.jpeg'
+                            text='Second hand Mercedez'
+                            label='Half price!'
+                            path='/services'
+                        />
+                    </ul>
+
+                    <ul className='cards__items'>
+                        <CardItem
+                            src='images/car3.jpeg'
+                            text='Second hand Audi'
+                            label='Min Downpayment!'
+                            path='/services'
+                        />
+                        <CardItem
+                            src='images/car4.jpeg'
+                            text='Second hand Chrysler'
+                            label='Best Deal'
+                            path='/products'
+                        />
+                        <CardItem
+                            src='images/car5.jpeg'
+                            text='Second hand Lexus'
+                            label='2017'
+                            path='/sign-up'
+                        />
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Cars;
