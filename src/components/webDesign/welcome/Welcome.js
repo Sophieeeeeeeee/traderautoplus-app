24c6f533abab0fdda170b1cf284ad075bb3375d0
Sophieeeeeeeee <sophiesun2810@gmail.com>
2021-11-30 17:53:06 -0500

    Fixed micro issue in Welcome.js

--- src/components/webDesign/welcome/Welcome.js
+++ src/components/webDesign/welcome/Welcome.js
@@ -3,6 +3,7 @@ import { Button } from "../button/Button";
 import "../../../app/App.css";
 import "./Welcome.css";
 import ReactPlayer from "react-player/youtube";
+import {constructor} from "react-xml-parser";
 
 /**
  * Renders Welcome (home) page with all the fancy buttons
@@ -14,55 +15,53 @@ class WelcomeSection extends Component {
 
   render() {
     return (
-      <div className="hero-container">
+        <div className="hero-container">
 
+          <section className="videoplayer">
+            <ReactPlayer
+                url="https://www.youtube.com/watch?v=RcQNgQFmLCI&ab_channel=RobComeau"
+                playing
+                loop
+                muted
+                width="100%"
+                height="80%"
+            />
+          </section>
 
-    render () {
-        return (
-            <div className='hero-container'>
+          <h1>Find your most suitable car, and loan.</h1>
 
-        <section className="videoplayer">
-          <ReactPlayer
-            url="https://www.youtube.com/watch?v=RcQNgQFmLCI&ab_channel=RobComeau"
-            playing
-            loop
-            muted
-            width="100%"
-            height="80%"
-          />
-        </section>
+          <p>Get Started Here</p>
 
-        <h1>Find your most suitable car, and loan.</h1>
+          <div className="hero-btns">
+            <Button
+                className="btns"
+                buttonStyle="btn--outline"
+                buttonSize="btn--large"
+                link="/sign-up"
+            >
+              GET STARTED
+            </Button>
 
-        <p>Get Started Here</p>
-
-        <div className="hero-btns">
-          <Button
-            className="btns"
-            buttonStyle="btn--outline"
-            buttonSize="btn--large"
-            link="/sign-up"
-          >
-            GET STARTED
-          </Button>
-
-          <Button
-            className="btns"
-            buttonStyle="btn--primary"
-            buttonSize="btn--large"
-            onClick={console.log("hey")}
-            link="/sign-in"
-          >
-            Sign In <i className="far fa-play-circle" />
-          </Button>
+            <Button
+                className="btns"
+                buttonStyle="btn--primary"
+                buttonSize="btn--large"
+                onClick={console.log("hey")}
+                link="/sign-in"
+            >
+              Sign In <i className="far fa-play-circle"/>
+            </Button>
+          </div>
         </div>
-      </div>
-    );
+    )
   }
 }
 
+
+
+
 class Welcome extends Component {
-  constructor(props) {
+  constructor(props){
     super(props);
     this.state = {};
   }
@@ -76,4 +75,4 @@ class Welcome extends Component {
   }
 }
 
-export default Welcome;
+export default Welcome
