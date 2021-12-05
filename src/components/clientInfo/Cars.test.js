a798204139bc6db0f2bf73f1a73ccdf72228fc46
elizabethhli <elizabethyli@outlook.com>
2021-12-05 18:44:13 -0500

    done CarItem and Cars testing

--- src/components/clientInfo/Cars.test.js
+++ src/components/clientInfo/Cars.test.js
@@ -7,16 +7,15 @@ import Cars from "./Cars";
 describe('Cars', () => {
     test('Test Cars page renders', async () => {
         render(<Cars />);
-        // TODO: test CarFilter appears?
-        // expect(screen.getByRole('CarList')).toBeInTheDocument();
         expect(screen.getByText('Check out these cars recommended to you!')).toBeInTheDocument();
         expect(screen.getByText('Apply filter to find the one for you!')).toBeInTheDocument();
-        expect(screen.getByRole('button', { name: /Filter/i})).toBeInTheDocument();
+        expect(screen.getByRole('button', { name: /filter/i})).toBeInTheDocument();
     });
 
     test('Test filter button', async () => {
+        render(<Cars />);
         // Test filter button; if never called there will be a timed-out failure
-        fireEvent.click(screen.getByRole('button', { name: /Filter/i}))
+        fireEvent.click(screen.getByRole('button', { name: /filter/i}))
     });
 
 });
