e5d7ae8f4747b11bb8db24c6f8334b4d637ca116
Ameen Parthab <ameen.parthab@mail.utoronto.ca>
2021-12-07 16:09:39 -0500

    --- changed video and decreased margin space --- Simply video and spacing change

--- src/components/webDesign/welcome/Welcome.js
+++ src/components/webDesign/welcome/Welcome.js
@@ -8,16 +8,16 @@ import ReactPlayer from "react-player/youtube";
  * Renders Welcome (home) page with all the fancy buttons
  */
 class WelcomeSection extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
   render() {
     return (
       <div className="hero-container">
         <section className="videoplayer">
           <ReactPlayer
-            url="https://www.youtube.com/watch?v=RcQNgQFmLCI&ab_channel=RobComeau"
+            url="https://www.youtube.com/watch?v=8m9vzffbae4"
             playing
             loop
             muted
@@ -26,9 +26,9 @@ class WelcomeSection extends Component {
           />
         </section>
 
-                <h1>Find your most suitable car, and loan.</h1>
+        <h1>Find your most suitable car, and loan.</h1>
 
-                <p>Get Started Here</p>
+        <p>Get Started Here</p>
 
         <div className="hero-btns">
           <Button
@@ -40,28 +40,23 @@ class WelcomeSection extends Component {
             Get Started
           </Button>
 
-
-                    <Button
-                        className="btns"
-                        buttonStyle="btn--primary"
-                        buttonSize="btn--large"
-                        onClick={console.log("hey")}
-                        link="/sign-in"
-                    >
-                        Sign In <i className="far fa-play-circle"/>
-                    </Button>
-                </div>
-            </div>
-        )
-    }
+          <Button
+            className="btns"
+            buttonStyle="btn--primary"
+            buttonSize="btn--large"
+            onClick={console.log("hey")}
+            link="/sign-in"
+          >
+            Sign In <i className="far fa-play-circle" />
+          </Button>
+        </div>
+      </div>
+    );
+  }
 }
 
-
-
-
-
 class Welcome extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {};
   }
@@ -75,4 +70,4 @@ class Welcome extends Component {
   }
 }
 
-export default Welcome
+export default Welcome;
